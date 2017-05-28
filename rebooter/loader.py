f23b0fad746d8c5d1ef30b7e28f0d8d474620383
Orien Zeng <ozeng@stanford.edu>
2017-05-28 06:14:41 +0000

    Working version with serial listing

--- rebooter/loader.py
+++ rebooter/loader.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 
-def reboot(pin):
+def reboot(pin, platformio_base, serialNumber):
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 	GPIO.setup(pin,GPIO.OUT)
@@ -14,9 +14,14 @@ def reboot(pin):
 	time.sleep(1)
 	GPIO.output(pin,GPIO.HIGH)
 	time.sleep(1)
-	subprocess.call("./teensy_loader_cli --mcu=TEENSY36 firmware.hex", shell=True)
+	#subprocess.call("./teensy_loader_cli --mcu=TEENSY36 firmware.hex", shell=True)
+	subprocess.call("cd " + platformio_base + " && platformio run && cd ../rebooter/ && ./teensy_loader_cli --mcu=TEENSY36 " + platformio_base + ".pioenvs/teensy36/firmware.hex", shell=True)
+	time.sleep(1)
+	#subprocess.call("screen -L /dev/ttyACM" + str(serialNumber) + " &")# + " > " + "ACM" + str(serialNumber) + ".txt &", shell=True)
 
-reboot(2) #master
-reboot(3)
-
-
+reboot(2, "../teensyMaster/", 0) #master
+reboot(3, "../stateMachine/", 1)
+#reboot(3, "../teensyMaster/") #master
+print("Serials available:")
+subprocess.call("ls /dev/ttyACM*", shell=True)
+#subprocess.call("screen -L /dev/ttyACM" + str(serialNumber) + " &")# + " > " + "ACM" + str(serialNumber) + ".txt &", shell=True)
