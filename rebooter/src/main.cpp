859cf074d7d90a77d4730563994829b4c7abc020
Orien Zeng <ozeng@stanford.edu>
2017-05-27 19:12:12 -0700

    Add include statement

--- rebooter/src/main.cpp
+++ rebooter/src/main.cpp
@@ -1,5 +1,15 @@
-void setup() {
-    pinMode(13, OUTPUT);
-    digitalWrite(13, HIGH);
+#include <Arduino.h>
+int ledPin = 13;                 // LED connected to digital pin 13
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);      // sets the digital pin as output
 }
-void loop(){}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(1000);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(1000);                  // waits for a second
+}
