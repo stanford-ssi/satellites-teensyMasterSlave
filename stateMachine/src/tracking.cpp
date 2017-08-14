3933bc1d48b915d8dea7b9e5f30f95ba3eb5742f
Stephen Spears <sspears@stanford.edu>
2017-08-14 13:32:18 -0700

    Added incoherent method, globals, and setup

--- stateMachine/src/tracking.cpp
+++ stateMachine/src/tracking.cpp
@@ -14,6 +14,15 @@ volatile uint64_t numLockedOn = 0;
 volatile uint64_t totalPowerReceivedBeforeIncoherent = 0;
 volatile uint64_t totalPowerReceived = 0;
 const uint64_t maxPower = 1ULL << 32;
+const int samples_per_cell = 32;
+const int numCells = 4;
+const int envelope[4] = {0,1,0,-1};
+//Stores each sample for each cell to use for the incoherent detection
+int32_t buff[samples_per_cell*numCells];
+//Keeps track of where in the rolling buffer I am
+int sample;
+//Stores the running sum of the previous four samples of each quad cell for both sine and cosine envelopes
+int32_t rolling_detectors[2*numCells];
 
 void sendOutput(mirrorOutput& output);
 
@@ -58,7 +67,35 @@ void leaveTracking() {
 }
 
 void incoherentProcess(const volatile adcSample& s, adcSample& output) {
-    output.copy(s);
+  //Retrieve latest sample values;
+  int32_t latest[numCells] = {s->axis1, s->axis2, s->axis3, s->axis4};
+
+  //Ensures the sample does not exceed the buffer index
+  sample = sample % samples_per_cell;
+
+  //Determines square wave value for our "sine" and "cosine" waves
+  int sinVal = envelope[sample%4];
+  int cosVal = envelope[(sample + 1)%4];
+
+  //Replaces previous adc sample with new sample, and saves the previous sample for each quad cell
+  for(int i = 0; i < numCells; i++){
+    int32_t previous = buff[sample * numCells + i];
+    buff[sample * numCells + i] = latest[i];
+
+    //Updates the rolling detector sums wihtout having to recalculate the entire function (subtracts previous and adds new)
+    rolling_detectors[i * 2] += sinVal*(latest[i] - previous);
+    rolling_detectors[i * 2 + 1] += cosVal*(latest[i] - previous);
+  }
+
+  sample++;
+
+  //Calculates and sets the four axis incoherent values
+  for(int i = 0; i < numCells; i++){
+    output->axis1 = sqrt(sq(rolling_detectors[0]/samples_per_cell) + sq(rolling_detectors[1]/samples_per_cell));
+    output->axis2 = sqrt(sq(rolling_detectors[2]/samples_per_cell) + sq(rolling_detectors[3]/samples_per_cell));
+    output->axis3 = sqrt(sq(rolling_detectors[4]/samples_per_cell) + sq(rolling_detectors[5]/samples_per_cell));
+    output->axis4 = sqrt(sq(rolling_detectors[6]/samples_per_cell) + sq(rolling_detectors[7]/samples_per_cell));
+  }
 }
 
 void pidProcess(const volatile adcSample& s) {
