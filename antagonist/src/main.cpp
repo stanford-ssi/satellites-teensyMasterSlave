60dc04732c3e69a12b532821e25d473af458907b
Orien Zeng <ozeng@stanford.edu>
2017-08-17 21:59:03 -0700

    Merge in michael's changes

--- antagonist/src/main.cpp
+++ antagonist/src/main.cpp
@@ -4,19 +4,19 @@
  * Michael Taylor
  * Aug 8th(ish) 2017
  *
- * This program generates a varying sinusoid in X and Y, initializes the SPI, initializes the DAC, sets a period (loop delay for now, will use timers later)
+ * This program generates a varying sinusoid in X and Y, initializes the SPI, initializes the DAC, sets a period
  * then plays back the sinusoid over the MEMS driver.
  */
-//#include <spi4teensy3.h>
 #include <SPI.h>
 #define BUFFER_LEN 1024
 static float sine_wave[BUFFER_LEN]; // buffer to hold decimal value sine wave (scaled to uint16_t before sent to DAC)
 static uint8_t DAC_write_word[3]; // DAC input register is 24 bits, SPI writes 8 bits at a time. Need to queue up 3 bytes (24 bits) to send every time you write to it
+// Teensy 3.2 pinouts
 const int slaveSelectPin = 10;
 const int FCLK_pin = 9;
 const int DRIVER_HV_EN_pin = 8;
-const int LASER_EN_PIN = 7;
-const int FILTER_CUTOFF_HZ = 100; //filter cutoff frequency in Hz
+const int LASER_EN_PIN = 6; // toggles laser on / off for modulation (PWM pin)
+const int FILTER_CUTOFF_HZ = 100; // MEMS driver signal filter cutoff frequency in Hz DO NOT SET ABOVE 100 HZ
 bool laser_state = LOW;
 int sample = 0;
 int count = 0;
@@ -33,7 +33,6 @@ uint16_t DAC_ch_D = 32768;
 float twopi = 2*3.14159265359; // good old pi
 float phase = twopi/BUFFER_LEN; // phase increment for sinusoid
 void setup() {
-// put your setup code here, to run once:
 // Setup Pins:
 pinMode (slaveSelectPin, OUTPUT); // SPI SS pin 10
 pinMode (FCLK_pin, OUTPUT); // Driver board filter clock pin 9
@@ -42,7 +41,7 @@ pinMode (LASER_EN_PIN, OUTPUT); // Laser diode switching pin 7, active low.
 // write pins low
 digitalWrite(slaveSelectPin,LOW);
 digitalWrite(DRIVER_HV_EN_pin,LOW);
-digitalWrite(LASER_EN_PIN,LOW);
+analogWrite(LASER_EN_PIN,0);
   // setup serial
   Serial.begin(19200);
   // setup SPI
@@ -82,6 +81,7 @@ digitalWrite(LASER_EN_PIN,LOW);
   delayMicroseconds(1);
 
   // Fill the sine wave buffer with 1024 points
+  // The 0.9 here prevents the driver from ever going over 150V! (It can do ~160 which can damage mirror)
   Serial.println("Filling in sine wave");
   for (int i = 0; i < BUFFER_LEN; i++){
     sine_wave[i] = 0.9*sin(i*phase); // fill 0 to twopi phase, output range -0.9 to 0.9 to limit HV output
@@ -90,10 +90,13 @@ digitalWrite(LASER_EN_PIN,LOW);
   // 50% duty cycle at desired filter cutoff = 60*desired frequency
   analogWriteFrequency(FCLK_pin, FILTER_CUTOFF_HZ*60);
   analogWrite (FCLK_pin, 128);
+  // Start the LASER PWM timer
+  // 50% duty cycle at desired frequency
+  analogWriteFrequency(LASER_EN_PIN, 1000);
+  analogWrite (LASER_EN_PIN,0);
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1);
+  delay(1); // running at ~1kHz, should be a timer and interrupt but I am lazy
   // figure out the sine wave output for each channel, from -1,1 to uint16_t centered at 32768
   // X = A - B
   DAC_ch_A = (uint16_t) (32768*(1 + (ampl_x/2)*sine_wave[sample]));
@@ -178,11 +181,11 @@ void loop() {
     if (byte_in == 'l') {
       if (laser_state == LOW){
       Serial.println("Laser On");
-      digitalWrite(LASER_EN_PIN,HIGH);
+      analogWrite (LASER_EN_PIN, 128);
       laser_state = HIGH;
       } else{
         Serial.println("Laser Off");
-        digitalWrite(LASER_EN_PIN,LOW);
+        analogWrite (LASER_EN_PIN, 0);
         laser_state = LOW;
       } // end else laser off
     } // endif laser on
