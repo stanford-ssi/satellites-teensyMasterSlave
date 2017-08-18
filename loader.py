f03699e7883d5f468d0fcf033ee1e16f07b8c8e6
Orien Zeng <ozeng@stanford.edu>
2017-08-17 20:48:28 -0700

    Working programming with fet

--- loader.py
+++ loader.py
@@ -3,15 +3,18 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 
-reset_master = 21
 program_master = 2
 program_slave = 3
+reset_master = 4
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(reset_master,GPIO.OUT)
 GPIO.setup(program_master,GPIO.OUT)
 GPIO.setup(program_slave,GPIO.OUT)
+GPIO.output(reset_master, GPIO.LOW) # No power to FET - antagonist
+GPIO.output(program_slave, GPIO.HIGH) # Not program mode
+GPIO.output(program_master, GPIO.HIGH) # Not program mode
 
 def cycle(pin):
     GPIO.output(pin, GPIO.HIGH)
@@ -28,22 +31,24 @@ def showSerial():
 
 def program(platformio_base, mcu):
     time.sleep(1)
-    command = "cd " + platformio_base + " && platformio run && cd ../ && ./teensy_loader_cli --mcu=" + mcu + " " + platformio_base + ".pioenvs/teensy36/firmware.hex"
+    command = "cd " + platformio_base + " && platformio run && cd ../ && ./teensy_loader_cli --mcu=TEENSY" + mcu + " " + platformio_base + ".pioenvs/teensy" + mcu + "/firmware.hex"
+    command = "./teensy_loader_cli --mcu=TEENSY" + mcu + " " + platformio_base + ".pioenvs/teensy" + mcu + "/firmware.hex"
     print(command)
     subprocess.call(command, shell=True)
     time.sleep(1)
     #subprocess.call("g++ spitest.cpp -std=c++11 -lwiringPi -o spitest", shell=True) # compile master mode
 
+
 cycle(program_slave)
-cycle(program_master)
-cycle(reset_master)
-print("Antagonist serial:")
+print("Slave serial:")
 subprocess.call("ls /dev/ttyACM*", shell=True)
-program("stateMachine/", "TEENSY36")
+program("stateMachine/", "36")
+GPIO.output(reset_master, GPIO.HIGH)
+time.sleep(1)
 print("Both serial:")
 subprocess.call("ls /dev/ttyACM*", shell=True)
 cycle(program_master)
-program("antagonist/", "TEENSY32")
+program("antagonist/", "31")
 showSerial()
 subprocess.call("./serial");
 #subprocess.call("g++ master.cpp -std=c++11 -lwiringPi -o master && ./master", shell=True) # compile master mode
